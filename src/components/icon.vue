4c477c355fc821d7b1abc0b41916b7dce9d33c75
Jasmine Cervantes <jt24cervantes@yahoo.com>
2017-09-21 10:11:07 -0600

    icon margin fixed

--- src/components/icon.vue
+++ src/components/icon.vue
@@ -4,27 +4,18 @@
       <b-row>
         <b-col>
           <a href="https://www.google.com/maps">
-          <b-img center src="https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Location_marker_pin_map_gps.png" width="100px" height="100px" />
+          <b-img center src="https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Location_marker_pin_map_gps.png" width="50px" height="50px" />
           </a>
-          <p center class="text">
-           Going somewhere? Go with Maps, real-time GPS navigation, traffic and transit.
-         </p>
         </b-col>
         <b-col>
           <a href="https://www.google.com/gmail/about/">
-          <b-img center src="http://www.freeiconspng.com/uploads/communication-email-2-icon-7.png" width="100px" height="100px" />
+          <b-img center src="http://www.freeiconspng.com/uploads/communication-email-2-icon-7.png" width="50px" height="50px" />
           </a>
-          <p center class="text">
-           Email on the go, manage your email, schedule, and attachments.
-          </p>
         </b-col>
         <b-col>
           <a href="https://calendar.google.com/calendar/render#main_7">
-          <b-img center src="http://www.oogazone.com/wp-content/uploads/best-hd-inland-northwest-bands-calendar-library.jpg" width="100px" height="100px" />
+          <b-img center src="http://www.oogazone.com/wp-content/uploads/best-hd-inland-northwest-bands-calendar-library.jpg" width="50px" height="50px" />
           </a>
-          <p center class="text">
-          Google's free online calendar, it's easy to keep track of life's important events all in one place.
-         </p>
         </b-col>
       </b-row>
     </b-container>
@@ -37,11 +28,9 @@ export default {
 
 }
 </script>
-
 <style>
-
-
 .icon {
-  padding: 80px;
+  margin: 5vh;
 }
+
 </style>
